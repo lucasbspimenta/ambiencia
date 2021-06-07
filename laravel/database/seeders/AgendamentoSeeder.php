4406f378c62657f25750df6b01c82a34e287d70d
Lucas Pimenta <lucas@clib.com.br>
2021-06-07 15:28:54 -0300

    Ajuste no Seeder para funcionar no servidor

--- laravel/database/seeders/AgendamentoSeeder.php
+++ laravel/database/seeders/AgendamentoSeeder.php
@@ -16,16 +16,23 @@ class AgendamentoSeeder extends Seeder
     public function run(Faker $faker)
     {
         $tipos = [1,1,1,1,2,3,4];
-        $agentes = User::get()->where('is_relog',true);
+        $agentes = User::get()->where('is_relog',true)->where('is_admin', false)->where('is_gestor', false);
+
+        echo 'Agentes fakes: ' . sizeof($agentes) . PHP_EOL;
 
         foreach($agentes as $agente)
         {
-            $unidades = $agente->unidades->toArray();
+            echo 'Agentes: ' . $agente->name . PHP_EOL;
+
+            $unidades = $agente->unidades()->take(50)->get()->toArray();
+
+            echo 'Unidades: ' . sizeof($unidades) . PHP_EOL;
+
             if(sizeof($unidades) > 0) {
                 //$unidades_selecionadas = array_rand($unidades, 20);
 
                 foreach($unidades as $idx => $unidade) {
-                    $data = $faker->dateTimeBetween($startDate = 'now', $endDate = '+90 days', $timezone = null)->format('Y-m-d');
+                    $data = $faker->dateTimeBetween($startDate = '-30 days', $endDate = '+90 days', $timezone = null)->format('Y-m-d');
 
                     $agendamento = [
                         'unidade_id' => $unidade['id'],
